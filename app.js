e2d0e3457285c47e5852a420e16164876c5ca5aa
CB <browchr4@oregonstate.edu>
2020-05-13 19:59:11 -0700

    finished

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//Chris Brown CS493 Assign6
+//Used GAE's nodejs-docs-samples, lectures, https://stackoverflow.com/questions/5643321/how-to-make-remote-rest-call-inside-node-js-any-curl, https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const request = require('request');
+
 let hbs = require('express-handlebars').create({
 	defaultLayout: 'main',
 	extname: 'hbs',
@@ -32,7 +22,8 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 const secret = '5_uCmjfrKpr4gXHq1RFsW6Bn';
-const redirectionBase = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=447522676163-jp2pi47rl10p8737mmenb1c2n3frhtdm.apps.googleusercontent.com&redirect_uri=https://cs493-assign5.wm.r.appspot.com/greetings&scope=profile&state=';
+const redirectionBase = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=447522676163-jp2pi47rl10p8737mmenb1c2n3frhtdm.apps.googleusercontent.com&redirect_uri=https://cs493-assign5.wm.r.appspot.com/greetings&scope=profile email openid&state=';
+const serverUri = 'https://cs493-assign5.wm.r.appspot.com'
 var state = ''; 
 var accessToken = '';
 
@@ -61,10 +52,44 @@ app.get('/createState', (req, res) => {
 
 //This page presents all the data it has received
 app.get('/greetings', (req, res) => {
-	if (req.query.state == state){
-        res.send('Successful Flow');
-    }
-    else {
+	if (req.query.state == state){                                          //If state matches it is a request from same client
+        var totalUri = serverUri + '/greetings';
+
+        const payload = {
+            code: req.query.code,
+            client_id: '447522676163-jp2pi47rl10p8737mmenb1c2n3frhtdm.apps.googleusercontent.com',
+            client_secret: secret,
+            redirect_uri: totalUri,
+            grant_type: 'authorization_code'
+        };
+        //Let's get that OAuth token
+        request.post({
+            uri: 'https://oauth2.googleapis.com/token',
+            json: true,
+            body: payload
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {                     //If state and secret are same then turns auth code into a token
+                accessToken = 'Bearer ' + body.access_token;                //Set global token in case we needed subsequent requests
+
+                request({                                                   //Use token to request People API url from Assignment hints
+                    uri: 'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses',
+                    json: true,
+                    headers: {'Authorization': accessToken}
+                }, function (error, response, body) {
+                    var fill = {};
+                    fill.firstName = body.names[0].givenName;
+                    fill.lastName = body.names[0].familyName;
+                    fill.curState = state;
+
+                    res.render('hello', fill);
+                });
+            }
+            else{
+                res.send('Something went wrong');
+            }
+        })
+    } 
+    else {                                                                  //If state doesn't match then ignore
         res.send('Fraud!')
     }
 });
